0f1ebe12d0da1d20f2da3ff7323075f16693b95b
xiepl1997 <xiepl1997@gmail.com>
2020-03-20 22:24:42 +0800

    update

--- src/main/java/com/xpllyn/XpllynApplication.java
+++ src/main/java/com/xpllyn/XpllynApplication.java
@@ -8,7 +8,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 @MapperScan("com.xpllyn.mapper")
-public class XpllynApplication {
+public class XpllynApplication extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(XpllynApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(XpllynApplication.class, args);
