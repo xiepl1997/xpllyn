b350c235dc212b50bce54927a79837ecbbc3be85
xiepl1997 <xiepl1997@gmail.com>
2020-07-11 20:57:56 +0800

    添加过滤器和拦截器

---
+++ src/main/java/com/xpllyn/configurer/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package com.xpllyn.configurer;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 拦截器
+ * 2020-6-26 Peiliang Xie
+ */
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    /**
+     * 在controller调用之前进行调用
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.sendRedirect(request.getContextPath() + "/");
+        return false;
+    }
+}
---
+++ src/main/java/com/xpllyn/configurer/MyWebConfigurer.java
@@ -0,0 +1,35 @@
+package com.xpllyn.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 过滤器
+ * 2020-6-26 Peiliang Xie
+ */
+@Configuration
+public class MyWebConfigurer implements WebMvcConfigurer {
+
+    /**
+     * 配置视图映射
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+    }
+
+    /**
+     * 配置拦截路径，拦截controller中的请求路径
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login", "/login/**", "/register", "static/**", "bootstrap-3.3.7-dist/**",
+                        "css/**", "font-awesome-4.7.0/**", "images/**", "js/**", "templates/**");
+    }
+}
